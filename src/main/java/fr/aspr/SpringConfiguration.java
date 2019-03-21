15267776ad8d50cef54fe348cfdbcd8e3e7728c9
Cojy <jouault.cyril@gmail.com>
2019-03-21 16:06:55 +0100

    adding latest files for jpa

--- src/main/java/fr/aspr/SpringConfiguration.java
+++ src/main/java/fr/aspr/SpringConfiguration.java
@@ -2,11 +2,26 @@ package fr.aspr;
 
 import java.util.Collections;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.hibernate.dialect.MariaDB103Dialect;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.zaxxer.hikari.HikariDataSource;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -19,7 +34,15 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 @EnableWebMvc
+@EnableJpaRepositories
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+@ComponentScan
 public class SpringConfiguration implements WebMvcConfigurer{
+	
+	@Value( "${test}" )
+	private String jdbcUrl;
+	
 	@Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
@@ -48,5 +71,68 @@ public class SpringConfiguration implements WebMvcConfigurer{
 	      new Contact("John Doe", "www.example.com", "myeaddress@company.com"), 
 	      "License of API", "API license URL", Collections.emptyList());
 	}
+	
+	//*
+	@Bean
+	public DataSource dataSource() {
+		
+		
+	//	 BasicDataSource dataSource = new BasicDataSource();
+
+	  //    dataSource.setDriverClassName("org.postgresql.Driver");
+	  //    dataSource.setUrl("jdbc:postgresql://localhost:5432/teste?charSet=LATIN1");
+	  //    dataSource.setUsername("root");
+	  //    dataSource.setPassword("root");
+	
+	    //  return dataSource;
+		
+		
+		final HikariDataSource ds = new HikariDataSource();
+        ds.setMaximumPoolSize(20);
+        ds.setDriverClassName("org.mariadb.jdbc.Driver");
+        ds.setJdbcUrl("jdbc:mariadb://localhost:3306/aspr");
+        ds.addDataSourceProperty("user", "root");
+        ds.addDataSourceProperty("password", "root");
+        ds.setAutoCommit(false);
+	    
+        return ds;
+	}
+
+	  @Bean
+	  public EntityManagerFactory entityManagerFactory() {
+
+	    HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	    vendorAdapter.setGenerateDdl(true);
+	    vendorAdapter.setDatabasePlatform("org.hibernate.dialect.MariaDB103Dialect");
+
+	    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+	    factory.setJpaVendorAdapter(vendorAdapter);
+	    factory.setPackagesToScan("fr.aspr");
+	    factory.setDataSource(dataSource());
+	    factory.afterPropertiesSet();
+	    
+	    //factory.setJpaDialect("org.hibernate.dialect.MariaDB103Dialect");
+
+	    return factory.getObject();
+	  }
+
+	  @Bean
+	  public PlatformTransactionManager transactionManager() {
+
+	    JpaTransactionManager txManager = new JpaTransactionManager();
+	    txManager.setEntityManagerFactory(entityManagerFactory());
+	    return txManager;
+	  }
+	  //*/
+
+		
+		/*
+		@Bean
+	   @Autowired
+	   public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+	      HibernateTransactionManager txManager = new HibernateTransactionManager();
+	      txManager.setSessionFactory(sessionFactory);
+	      return txManager;
+	   }//*/
 }
 
--- src/main/java/fr/aspr/webservices/UserService.java
+++ src/main/java/fr/aspr/webservices/UserService.java
@@ -1,18 +1,56 @@
 package fr.aspr.webservices;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import fr.aspr.exceptions.ForbiddenException;
+import fr.aspr.model.User;
+import fr.aspr.repositories.UserRepository;
 
 @RestController
 @RequestMapping("/users")
 public class UserService {
 
+	@Autowired
+	private UserRepository ur;
+	
 	@CrossOrigin
-	@GetMapping("/test")
-	public String getTest() {
-		return "OK";
+	@GetMapping("/")
+	public Iterable<User> getAllUsers() {
+		return ur.findAll();
+	}
+	
+	@CrossOrigin
+	@GetMapping("/{id}")
+	public ResponseEntity<User> getById(
+			@PathVariable("id") long id) {
+		Optional<User> opt = ur.findById(id);
+		if(opt.isPresent())
+			return new ResponseEntity<User>(opt.get(), HttpStatus.OK);
+		
+		return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
+	}
+	
+	@CrossOrigin
+	@GetMapping
+	public User getByName(
+			@RequestParam(value="name", required=true) String name) {
+		
+		//this will not happen, it's an example if the name had required=false
+		if(name ==null || name.isEmpty())
+			throw new ForbiddenException();
+		
+		return ur.findByName(name);		
+		
 	}
 	
 }
